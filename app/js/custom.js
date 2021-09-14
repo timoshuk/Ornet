ec9eeffa1358b41885731347c697b190fe1d4477
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2021-09-14 23:50:40 +0300

    add slider

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,13 +1,42 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  var swiper = new Swiper(".swiper", {
-    // Optional parameters
-    direction: "vertical",
-    loop: true,
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination"
-    }
-  });
-});
+  var navBtn = document.getElementById("goods__nav-btn");
+  var goodsNav = document.querySelector(".goods__nav-list");
+  var goodsNavItems = document.querySelectorAll(".goods__nav-link");
+  clickEventsHendler(); // Show mobile nav
+
+  function showNav() {
+    navBtn.classList.toggle("goods__nav-btn--open");
+    goodsNav.classList.toggle("goods__nav-list--show");
+  }
+
+  function clickEventsHendler() {
+    navBtn.addEventListener("click", function () {
+      showNav();
+    });
+    goodsNavItems.forEach(function (itemLink) {
+      itemLink.addEventListener("click", function () {
+        showNav();
+      });
+    });
+  }
+
+  function swiperInit() {
+    var swiper = new Swiper(".swiper", {
+      // Optional parameters
+      direction: "vertical",
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 200,
+      centeredSlidesBounds: true,
+      effect: "slide",
+      // If we need pagination
+      pagination: {
+        el: ".swiper-pagination"
+      }
+    });
+  }
+
+  swiperInit();
+}); // swiper-slide-active
--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -1,12 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    direction: "vertical",
-    loop: true,
+  const navBtn = document.getElementById("goods__nav-btn");
+  const goodsNav = document.querySelector(".goods__nav-list");
+  const goodsNavItems = document.querySelectorAll(".goods__nav-link");
 
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
+  clickEventsHendler();
+
+  // Show mobile nav
+  function showNav() {
+    navBtn.classList.toggle("goods__nav-btn--open");
+    goodsNav.classList.toggle("goods__nav-list--show");
+  }
+
+  function clickEventsHendler() {
+    navBtn.addEventListener("click", () => {
+      showNav();
+    });
+
+    goodsNavItems.forEach((itemLink) => {
+      itemLink.addEventListener("click", () => {
+        showNav();
+      });
+    });
+  }
+
+  function swiperInit() {
+    const swiper = new Swiper(".swiper", {
+      // Optional parameters
+      direction: "vertical",
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 200,
+      centeredSlidesBounds: true,
+      effect: "slide",
+
+      // If we need pagination
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+  }
+
+  swiperInit();
 });
+
+// swiper-slide-active
